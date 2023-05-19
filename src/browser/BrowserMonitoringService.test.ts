bf40e1df28c0a0fd841014b17af3a19377c04f1f
Illia Lukianov <illia.lukianov@kilo.health>
2023-05-19 16:36:01 +0800

    chore: improve tests for browser monitoring and remove extra console

--- src/browser/BrowserMonitoringService.test.ts
+++ src/browser/BrowserMonitoringService.test.ts
@@ -14,17 +14,19 @@ describe('BrowserMonitoringService', () => {
   describe('constructor', () => {
     let consoleInfoSpy: jest.SpyInstance;
     beforeEach(() => {
-      consoleInfoSpy = jest.spyOn(console, 'info');
+      consoleInfoSpy = jest.spyOn(console, 'info').mockImplementation();
     });
 
     it('should initialize with console if no params provided', () => {
+      const context = { key: 'value' };
       const browserMonitoringService = new BrowserMonitoringService();
 
-      browserMonitoringService.info('test');
-      expect(consoleInfoSpy).toHaveBeenCalledWith('test');
+      browserMonitoringService.info('test', context);
+      expect(consoleInfoSpy).toHaveBeenCalledWith('test', context);
     });
 
     it('should initialize with console if not enough params for remote logging provided', () => {
+      const context = { key: 'value' };
       const browserMonitoringService = new BrowserMonitoringService({
         serviceName: 'serviceName',
         serviceEnv: 'serviceEnv',
@@ -32,11 +34,12 @@ describe('BrowserMonitoringService', () => {
         clientToken: '',
       });
 
-      browserMonitoringService.info('test');
-      expect(consoleInfoSpy).toHaveBeenCalledWith('test');
+      browserMonitoringService.info('test', context);
+      expect(consoleInfoSpy).toHaveBeenCalledWith('test', context);
     });
 
     it('should initialize with datadog logs if params provided', () => {
+      const context = { key: 'value' };
       const browserMonitoringService = new BrowserMonitoringService({
         serviceName: 'serviceName',
         serviceEnv: 'serviceEnv',
@@ -44,11 +47,11 @@ describe('BrowserMonitoringService', () => {
         clientToken: 'clientToken',
       });
 
-      browserMonitoringService.info('test');
-      expect(datadogLogs.logger.info).toHaveBeenCalledWith('test', undefined);
+      browserMonitoringService.info('test', context);
+      expect(datadogLogs.logger.info).toHaveBeenCalledWith('test', context);
     });
 
-    beforeEach(() => {
+    afterEach(() => {
       consoleInfoSpy.mockRestore();
     });
   });
