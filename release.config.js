c974fa2e980f0c6465e16c1ddf56ec6294e693f0
Illia Lukianov <illia.lukianov@kilo.health>
2023-05-22 13:40:20 +0800

    chore: apply code review comments

--- release.config.js
+++ release.config.js
@@ -1,4 +1,57 @@
-const { semanticReleaseConfig } = require('@kilojs/semantic-release-config');
+const defaultParams = {
+  plugins: {
+    '@semantic-release/npm': { npmPublish: false },
+    '@semantic-release/git': {
+      message:
+        // eslint-disable-next-line no-template-curly-in-string
+        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  },
+  defaultBranch: 'main',
+};
+
+// TODO: remove after outsourcing @kilojs/semantic-release-config
+// this is copy paste from there
+export function semanticReleaseConfig(params) {
+  return {
+    branches: [
+      '+([0-9])?(.{+([0-9]),x}).x',
+      params.defaultBranch ?? defaultParams.defaultBranch,
+      'next',
+      'next-major',
+      { name: 'beta', prerelease: true },
+      { name: 'alpha', prerelease: true },
+    ],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/gitlab',
+      [
+        '@semantic-release/npm',
+        {
+          ...(defaultParams.plugins
+            ? defaultParams.plugins['@semantic-release/npm']
+            : undefined),
+          ...(params.plugins
+            ? params.plugins['@semantic-release/npm']
+            : undefined),
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          ...(defaultParams.plugins
+            ? defaultParams.plugins['@semantic-release/git']
+            : undefined),
+          ...(params.plugins
+            ? params.plugins['@semantic-release/git']
+            : undefined),
+        },
+      ],
+    ],
+  };
+}
 
 module.exports = {
   ...semanticReleaseConfig({
