cd0fad590fb5dcc9338e5c90dfea52654b5c4347
Illia Lukianov <illia.lukianov@kilo.health>
2023-05-23 11:16:59 +0800

    chore: try first publish public

--- release.config.js
+++ release.config.js
@@ -21,6 +21,7 @@ function semanticReleaseConfig(params) {
       'next-major',
       { name: 'beta', prerelease: true },
       { name: 'alpha', prerelease: true },
+      'feat/browser-monitoring-service',
     ],
     plugins: [
       '@semantic-release/commit-analyzer',
