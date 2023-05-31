4cdbe4980d51004889469e82bfb928761738d8d8
Illia Lukianov <illia.lukianov@kilo.health>
2023-05-31 19:08:19 +0800

    feat(server,client): make all RemoteMonitoringServiceParams optional

--- src/shared/MonitoringService.ts
+++ src/shared/MonitoringService.ts
@@ -2,10 +2,10 @@ import { Logger } from './Logger';
 import { ConsoleLogger } from './ConsoleLogger';
 
 export interface RemoteMonitoringServiceParams {
-  serviceName: string;
-  serviceVersion: string;
-  serviceEnv: string;
-  authToken: string;
+  serviceName?: string;
+  serviceVersion?: string;
+  serviceEnv?: string;
+  authToken?: string;
 }
 
 export abstract class MonitoringService {
