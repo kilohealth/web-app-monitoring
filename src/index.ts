17f9014f7fdd332a3d6f2a02a0f51f8958d044f4
illia.lukianov <illia.lukianov@kilo.health>
2023-06-16 13:01:01 +0800

    feat: add single entry point for all but tracing,
    BREAKING CHANGE: add exports, so dist folder won't work anymore when importing

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { BrowserMonitoringService } from './browser/BrowserMonitoringService';
+export { initServerMonitoring } from './server/initServerMonitoring';
+export { ServerMonitoringService } from './server/ServerMonitoringService';
